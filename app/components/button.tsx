fa1b20c44d197611434ccd7600c1052411a6e434
Dawid Bieńkowski <marksoft1992@gmail.com>
2023-11-21 08:45:06 +0100

    Ui components for login and register, components input and button

---
+++ app/components/button.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { IconType } from "react-icons";
+
+interface ButtonProps {
+  label: string;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  outline?: boolean;
+  small?: boolean;
+  icon?: IconType;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  label,
+  onClick,
+  disabled,
+  outline,
+  small,
+  icon: Icon,
+}) => {
+  return (
+    <button disabled={disabled} onClick={onClick}>
+      {Icon && (
+        <Icon
+          size={24}
+          className="
+            absolute
+            left-4
+            top-3
+          "
+        />
+      )}
+      {label}
+    </button>
+  );
+};
+
+export default Button;
